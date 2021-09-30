e040a1fc34b360f3408cd416a134bb4596962a55
yabouseada <y.abouseada@gmail.com>
2021-09-30 14:12:49 +0200

    starting code

---
+++ resources/user.py
@@ -0,0 +1,98 @@
+
+from blacklist import BLACKLIST
+from werkzeug.security import safe_str_cmp
+from flask_jwt_extended import (
+    create_access_token,
+    create_refresh_token,
+    jwt_required,
+    get_jwt_identity,
+    get_jti,
+    get_jwt
+)
+from flask_restful import Resource, reqparse
+from models.user import UserModel
+
+_user_parser = reqparse.RequestParser()
+_user_parser.add_argument('username',
+                type=str,
+                required=True,
+                help="This field cannot be left blank!"
+    )
+        
+_user_parser.add_argument('password',
+                type=str,
+                required=True,
+                help="This field cannot be left blank!"
+    )
+    
+
+class UserRegister(Resource):
+
+    def post(self):
+        data = _user_parser.parse_args()
+
+        
+        if UserModel.find_by_username(data['username']):
+            return {"message":"Username already exists"},400
+        
+        user = UserModel(**data)
+        user.save_to_db()
+
+        return{"message":"User created successfully."}, 201
+
+
+class UserReview(Resource):
+    def get(self):
+        
+            return {'username':[user.json() for user in UserModel.query.all()]}
+
+
+class User(Resource):
+    @classmethod
+    def get(cls,user_id):
+        user = UserModel.find_by_id(user_id)
+        if not user : 
+            return {'message':'User not found'}, 404
+        return user.json()
+
+    def delete(cls,user_id):
+        user = UserModel.find_by_id(user_id)
+        if not user : 
+            return {'message':'User not found'}, 404
+        user.delete_from_db()
+        return{'message': 'user deleted'}
+    
+
+class UserLogin(Resource):
+ 
+    @classmethod
+    def post(cls):
+        data = _user_parser.parse_args()
+
+        user = UserModel.find_by_username(data['username'])
+
+        if user and safe_str_cmp(user.password,data['password']):
+            access_token = create_access_token(identity=user.id , fresh = True)
+            refresh_token = create_refresh_token(user.id)
+            return {
+                'access_token':access_token,
+                'refresh_token':refresh_token
+            },200
+        
+        return {'message': ' Invalid credentials'},401
+
+
+class UserLogout(Resource):
+    @jwt_required()
+    def post(self):
+        jti = get_jwt()['jti']
+        BLACKLIST.add(jti)
+        return{'message':'Successfully logged out'},200
+
+
+class TokenRefresh(Resource):
+    @jwt_required(refresh=True)
+    def post(self):
+        current_user = get_jwt_identity()
+        new_token = create_access_token(identity=current_user,fresh=False)
+        return {'access_token': new_token},200
